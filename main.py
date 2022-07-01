4eb23fda1bc2cc584feb115193c46d531f26a88d
awacs <milad.liri@gmail.com>
2022-07-01 23:10:34 +0430

    main.py updated

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from DataBase.Sqlite import Database
+import sys
+from PyQt5.QtWidgets import QApplication
+from Window import Window
+
 
 Database.Initialize()
 
 
+app = QApplication(sys.argv)
+window = Window()
+sys.exit(app.exec_())
