9c29590a97fe59a18e6e623003f43c5ef8c4ab10
awacs <milad.liri@gmail.com>
2022-07-07 03:30:42 +0430

    email added

---
+++ Lib/Email.py
@@ -0,0 +1,20 @@
+import smtplib
+import ssl
+
+
+class Email:
+
+    SmtpServer = "smtp.gmail.com"
+    SenderEmail = "ap.4002.final.project.group2@gmail.com"
+    SenderPassword = "aA!123456789"
+    Port = 456
+
+
+    @staticmethod
+    def Send(receiverEmail : str, message : str):
+        """send email"""
+
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(Email.SmtpServer, Email.Port, context = context) as server:
+            server.login(Email.SenderEmail, Email.SenderPassword)
+            server.sendmail(Email.SenderEmail, receiverEmail, message)
